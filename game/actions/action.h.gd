6cd81fd586f3cfa9ed46e8390258eab8903bc2bb
Greg Solo <soloschenko@gmail.com>
2024-04-06 21:02:03 -0400

    Saving and loading config for actions and conditions

--- game/actions/action.h.gd
+++ game/actions/action.h.gd
@@ -6,7 +6,7 @@ class_name IAction
 @export var variable_id: int
 @export var value: bool
 
-func _init(_id: String, _variable_id: int, _value: bool) -> void:
+func _init(_id: String = "", _variable_id: int = 0, _value: bool = false) -> void:
     self.id = _id
     self.variable_id = _variable_id
     self.value = _value
--- game/conditions/condition.h.gd
+++ game/conditions/condition.h.gd
@@ -6,7 +6,7 @@ class_name ICondition
 @export var variable_id: int
 @export var value: bool
 
-func _init(_id: String, _variable_id: int, _value: bool) -> void:
+func _init(_id: String = "", _variable_id: int = -1, _value: bool = false) -> void:
     self.id = _id
     self.variable_id = _variable_id
     self.value = _value
